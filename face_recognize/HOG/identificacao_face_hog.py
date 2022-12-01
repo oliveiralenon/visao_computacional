c53a07c918d3a8e295e9f078ec68232fc1450f96
unknown <lenon-oliveira@hotmail.com>
2022-12-01 16:48:21 -0300

    codigos de reconhecimento facial

---
+++ face_recognize/HOG/identificacao_face_hog.py
@@ -0,0 +1,29 @@
+import cv2
+import dlib
+
+video_capture = cv2.VideoCapture(0)
+
+detector_face_hog = dlib.get_frontal_face_detector()
+
+
+while True:
+    # Captura frame pro frame
+    ok, frame = video_capture.read()
+
+    imagem_cinza = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    deteccoes = detector_face_hog(imagem_cinza, 1)
+
+    # Desenha o retângulo
+    for face in deteccoes:
+        l, t, r, b = face.left(), face.top(), face.right(), face.bottom()
+        cv2.rectangle(frame, (l, t), (r, b), (0, 255, 255), 2)
+
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+#Libera memória no final
+video_capture.release()
+cv2.destroyAllWindows()
